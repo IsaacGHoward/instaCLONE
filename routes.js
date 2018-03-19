82d1e9eba553e7489a5dbc9fee4966878809b87f
EiliseMcniff <eilise.mcniff.em@gmail.com>
2018-03-19 13:18:44 -0700

    signup progress

--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-	
+
 var express = require("express");
 var router = express.Router();
 var path = require("path");
@@ -6,27 +6,27 @@ var clientSessions = require('client-sessions');
 
 
 router.get("/signup",function(req,res){
-//add or modify.  Send back to the client signup.html.	
-	
+//add or modify.  Send back to the client signup.html.
+res.sendFile(__dirname + "/public/views/signup.html");
 });
 
 
 router.get("/",function(req,res){
-//add or modify.  Send back to the client login.html.	
+//add or modify.  Send back to the client login.html.
 	res.sendFile(__dirname + "/public/views/login.html");
 });
 
 
 router.get("/login",function(req,res){
-//add or modify.  Send back to the client login.html.	
+//add or modify.  Send back to the client login.html.
 	res.sendFile(__dirname + "/public/views/login.html");
 });
 
 //add or modify.  Below is already done for you.
-router.get("/logout",function(req,res){	
+router.get("/logout",function(req,res){
 	req.session_state.reset();
 	res.json({redirect:"/login"});
-});	
+});
 
 
 router.get("/session",function(req,res){
@@ -35,21 +35,21 @@ router.get("/session",function(req,res){
 //                else send back to the client login.html.
 		if(req.session_state.username)
 		{
-			res.sendFile(__dirname + "/public/views/session.html");	
+			res.sendFile(__dirname + "/public/views/session.html");
 		}
 		else
-			res.sendFile(__dirname + "/public/views/session.html");	
+			res.sendFile(__dirname + "/public/views/session.html");
 });
 
 router.get("/userInfo",function(req,res){
 //add or modify.  Look at req.session_state.??? to check if a session is active.
-//                If session is active then send back to client a json object 
+//                If session is active then send back to client a json object
 //                   with the user data.
 //                else send back a json object that is null.
 
 });
 
- 
+
 
 let userInfo = [];
 
@@ -59,7 +59,23 @@ router.post('/signup', function(req, res){
 //                  Give req.session_state.??? a valid value.
 //                  Send back a json object of {redirect:"/session"}.
 //                else send back a json object that is null.
+//eilise: link up to mydatabase module
+//add conditionals in login
 
+console.log("signup");
+if (req.body.username == "" || req.body.password == "") {
+res.json(null);
+return;
+}
+else{
+	userInfo.push({username:req.body.username, password:req.body.password})
+
+	req.session_state.username = req.body.username;
+	req.session_state.password = req.body.password;
+	//eilise: not neccesary currently
+	//may come in handy later
+	res.json({redirect:"/login"});
+}
 });
 
 
@@ -69,12 +85,22 @@ router.post('/login', function(req, res){
 //                  set req.session_state.??? to a valid value.
 //                  Send back a json object of {redirect:"/session"}.
 //                else send back a json object that is null.
-		console.log("We in here");
+		console.log("login");
+		//for(var i = 0; i < userInfo.length; i++)
+		//{
 		if (req.body.username == "" || req.body.password == "") {
 		res.json(null);
 		return;
 		}
-		else{
+//	else {
+//for(var i = 0; i < userInfo.length; i++)
+//{
+// if(req.body.username == userInfo[i].username)
+//		req.session_state.username = req.body.username;
+//		res.json({redirect:"/session"});
+//		}
+//}
+		else {
 		req.session_state.username = req.body.username;
 		res.json({redirect:"/session"});
 		}
@@ -83,4 +109,3 @@ router.post('/login', function(req, res){
 
 
 module.exports = router;
-
