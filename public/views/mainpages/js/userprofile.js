f6ee9ba7c073b4026973de934195063335d9dc13
IsaacGHoward <howardisaac100@gmail.com>
2018-04-08 19:13:51 -0700

    user profiles work!
    each profile has the user's name, real name, and list of posts :D

--- public/views/mainpages/js/userprofile.js
+++ public/views/mainpages/js/userprofile.js
@@ -10,25 +10,26 @@ function logoutClicked(){
 function sessionSuccess(User){
 	user = User;
 	console.log(user.username);
+	$('#name').html("Name: " + user.realname);
 	//$('#username').html(user.username+"'s"+" profile");
 	//$('#name').html("Name: " + user.realname);
 	//$('#age').html("DOB: " + user.age);
 	//$('datejoined').html("Date Joined: " + user.sd);
 
-	//let loc;
-  //var userposts = user.postObjects
-	//userposts.sort(function(a, b) {
-	//	return b.timestamp - a.timestamp;
-	//});
-	//console.log("/public/images/" + String("bison.jpg") +"");
-	//for (let i=0;userposts.length;i++) {
-	//	loc = userposts[i].imageName;
+	let loc;
+  var userposts = user.postObjects
+	userposts.sort(function(a, b) {
+		return b.timestamp - a.timestamp;
+	});
+	console.log("/public/images/" + String("bison.jpg") +"");
+	for (let i=0;userposts.length;i++) {
+		loc = userposts[i].imageName;
 
-	//$('#postlist').append($('<img>',{src:'/public/images/' + String(loc)}));
-	//$('#postlist').append("<br>");
-	//$('#postlist').append(userposts[i].caption);
-	//$('#postlist').append("<br><br><br><hr><br><br><br>");
-	//}
+	$('#postlist').append($('<img>',{src:'/public/images/' + String(loc)}));
+	$('#postlist').append("<br>");
+	$('#postlist').append(userposts[i].caption);
+	$('#postlist').append("<br><br><br><hr><br><br><br>");
+	}
 
 	//$('#postlist').append(user.postObjects[0].image);
 
@@ -52,12 +53,12 @@ $(document).ready(function(){
 		//c = unescape(temp[1]);
 		//c= c.split('_').join(' ');
 		//l = l.split('_').join(' ');
-		console.log(l);
+		console.log(" L : " + l);
 		//console.log(c);
 		//$("#postimage").attr("src",c);
 		$("#username").html(l+"'s Profile");
-		$.get('/getuserinfo',{'name' : "1"},sessionSuccess);
-
+		$.get('/getuserinfo',{'name' : l},sessionSuccess);
+		//console.log("USERNAME : " + user.username);
 		//document.getElementById("log").innerHTML = l;
 		//document.getElementById("pass").innerHTML = c;
 	}
