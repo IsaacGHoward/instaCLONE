ba9dec96371d344d542f03c29d39f8c53ee90e32
Eilise McNiff <eilise.mcniff.em@gmail.com>
2018-05-28 18:06:21 -0700

    mongo and  passport implemented

--- models/User.js
+++ models/User.js
@@ -1,36 +1,61 @@
+var bcrypt = require("bcrypt-nodejs");
+var SALT_FACTOR = 10;
+
 var mongoose = require("mongoose");
 
-var userSchema =  mongoose.Schema({
-	username: {
-		required: true,
-		unique: true,
-		type:String
-	},
-  password: {
-    required: true,
-    unique: false,
-    type:String
-  },
-  realname: {
-    required: true,
-    unique: false,
-    type:String
-  },
-  age: {
-    required: false,
-    unique: false,
-    type:Number
-  },
-  sd: {
-    required: false,
-    unique: false,
-    type:String
-  },
-  postObjects:[],
-  friendList:[],
-  userMsgHist : []
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  realname: { type: String, required: false },
+  age: { type: String, required: false },
+  sd: { type: String, required: true }
 });
 
-var User = mongoose.model('User', userSchema);
+
+var noop = function() {};
+
+userSchema.pre("save", function(done) {
+  console.log("userSchema.pre save");
+  var user = this;
+
+  if (!user.isModified("password")) {
+   console.log("userSchema.pre save not modified");
+   return done();
+  }
+  console.log("userSchema.pre bcrypt");
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+
+    if (err) { return done(err); }
+
+    bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
+      if (err) { return done(err); }
+  console.log("userSchema.pre set password to hashedPassword");
+      user.password = hashedPassword;
+      done();
+    });
+
+
+  });
+});
+
+userSchema.methods.checkPassword = function(guess, done) {
+    console.log("userSchema.methods.checkPassword");
+  bcrypt.compare(guess, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+userSchema.methods.name = function() {
+  return this.displayName || this.username;
+};
+
+var User;
+
+if (mongoose.models.User) {
+  User = mongoose.model('User');
+} else {
+  User = mongoose.model('User', userSchema);
+}
 
 module.exports = User;
